f12d25a8a9cf99e26607433de0d982fb0ef65f41
Biozard01 <arthur.laforest33@gmail.com>
2021-03-29 11:20:53 +0200

    yolo

--- login.js
+++ login.js
@@ -1,12 +1,12 @@
-const message = document.getElementById('message');
+const message = document.getElementById("message");
 const param = new URLSearchParams(window.location.search);
-const username = param.get('username');
-const pw = param.get('pw');
+const username = param.get("username");
+const pw = param.get("pw");
 
-if(username.toLowerCase() === 'codecademy' && pw === 'ilovecoding'){
-  message.innerHTML = 'We love coding too!';
-} else if(!username || !pw){
-  message.innerHTML = 'Add some client-side validation!';
+if (username.toLowerCase() === "codecademy" && pw === "ilovecoding") {
+  message.innerHTML = "We love coding too!";
+} else if (!username || !pw) {
+  message.innerHTML = "Add some client-side validation!";
 } else {
-  message.innerHTML = 'Hurray for client-side validation!';
+  message.innerHTML = "Hurray for client-side validation!";
 }
