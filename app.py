339a13b64883e2cab4b8ed2db4b7507eeca22c90
Biozard01 <arthur.laforest33@gmail.com>
2021-04-11 17:45:24 +0200

    working site

--- app.py
+++ app.py
@@ -26,26 +26,9 @@ def form():
     return render_template("signup.html")
 
 
-@app.route("/login", methods=["POST", "GET"])
+@app.route("/login")
 def login():
-    if request.method == "GET":
-        return render_template("405.html")
-
-    if request.method == "POST":
-        name = request.form["nom"]
-        password = request.form["password"]
-        email = request.form["email"]
-        cursor = mysql.connection.cursor()
-        cursor.execute(
-            """ INSERT INTO users FROM flask VALUES(%s,%s,%s)""",
-            (name, password, email),
-        )
-
-        mysql.connection.commit()
-        cursor.close()
-        return f"Done!!"
-
-    return render_template(login.html)
+    return render_template("login.html")
 
 
 @app.route("/logout")
